44838acdbbe97f11dece61a2aa4a1897e78e70cf
Trent Fehl <trentonfehl@gmail.com>
2019-07-29 05:50:28 +0100

    talking!

--- buttons.py
+++ buttons.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
 import signal
 import sys
+import socket
 import buttonshim
+from enum import IntEnum
+
+class Mode(IntEnum):
+    DEFAULT = 0
+    GHOST = 1
+    GLASSES = 2
 
 class ButtonWriter(object):
 
+    # Change the host to match the IP address to match the connected device.
     def __init__(self, host='localhost', port=8080):
         self._sock = socket.create_connection((host, port))
 
     def send(self, data):
         self._sock.sendall(data)
 
+writer = ButtonWriter()
+
 @buttonshim.on_press(buttonshim.BUTTON_A)
 def button_a(button, pressed):
-    writer = ButtonWriter()
-    writer.send(Mode.DEFAULT)
+    writer.send(Mode.DEFAULT.name.encode())
 
 @buttonshim.on_press(buttonshim.BUTTON_B)
 def button_b(button, pressed):
-    writer = ButtonWriter()
-    writer.send(Mode.GHOST)
+    writer.send(Mode.GHOST.name.encode())
 
 @buttonshim.on_press(buttonshim.BUTTON_C)
 def button_c(button, pressed):
-    writer = ButtonWriter()
-    writer.send(Mode.GLASSES)
+    writer.send(Mode.GLASSES.name.encode())
 
 def signal_handler(sig, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
+signal.pause()
